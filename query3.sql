9bf3adc26d3f88fe4aed3c27d58968fa6718a3f2
nmartinse <100205683+nmartinse@users.noreply.github.com>
2022-04-14 17:44:37 +0200

    Delete query3.sql

--- query3.sql
+++
@@ -1,5 +0,0 @@
-SELECT  Mutacion.Tipo, Gen.Nombre, Proteina.Nombre, Mutacion.Efecto_Fenotipo
-FROM Mutacion,Gen,Proteina
-WHERE Gen.ID = 102545
-AND Proteina.Gen_ID= Gen.ID
-AND Mutacion.Tipo= "Splicing";
