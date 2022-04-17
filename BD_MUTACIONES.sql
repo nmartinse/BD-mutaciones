c00213beda69fd7e622e18165b80822d5dde6d2b
nmartin <nmartins@uma.es>
2022-04-17 14:11:04 +0200

    actualizacion 17/04 14.10

--- BD_MUTACIONES.sql
+++ BD_MUTACIONES.sql
@@ -7,22 +7,22 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `mydb` ;
 
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
--- -----------------------------------------------------
--- Schema chebi
--- -----------------------------------------------------
 USE `mydb` ;
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Efecto`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`Efecto` ;
+
 CREATE TABLE IF NOT EXISTS `mydb`.`Efecto` (
   `Fenotipo` VARCHAR(100) NOT NULL,
-  `Descripcion` VARCHAR(500) NULL DEFAULT NULL,
+  `Descripcion` VARCHAR(500) NULL,
   PRIMARY KEY (`Fenotipo`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
@@ -31,13 +31,15 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- -----------------------------------------------------
 -- Table `mydb`.`Gen`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`Gen` ;
+
 CREATE TABLE IF NOT EXISTS `mydb`.`Gen` (
   `ID` INT NOT NULL,
-  `ID_ncbi` INT NULL DEFAULT NULL,
-  `Longitud` DOUBLE NULL DEFAULT NULL,
-  `Nombre` VARCHAR(50) NULL DEFAULT NULL,
-  `Posicion` VARCHAR(45) NULL DEFAULT NULL COMMENT 'Posición en el formato:\\\\\\\\nCROMOSOMA: POSICION ',
-  `Funcion` VARCHAR(45) NULL DEFAULT NULL,
+  `ID_ncbi` INT NULL,
+  `Longitud` DOUBLE NULL,
+  `Nombre` VARCHAR(50) NULL,
+  `Posicion` VARCHAR(45) NULL COMMENT 'Posición en el formato:\\\\nCROMOSOMA: POSICION ',
+  `Funcion` VARCHAR(45) NULL,
   PRIMARY KEY (`ID`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
@@ -46,10 +48,12 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- -----------------------------------------------------
 -- Table `mydb`.`Mutacion`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`Mutacion` ;
+
 CREATE TABLE IF NOT EXISTS `mydb`.`Mutacion` (
   `ID` VARCHAR(45) NOT NULL,
-  `Codon_afectado` VARCHAR(45) NULL DEFAULT NULL,
-  `Tipo` VARCHAR(45) NULL DEFAULT NULL,
+  `Codon_afectado` VARCHAR(45) NULL,
+  `Tipo` VARCHAR(45) NULL,
   `Gen_ID` INT NOT NULL,
   `Efecto_Fenotipo` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`ID`, `Gen_ID`, `Efecto_Fenotipo`),
@@ -66,16 +70,60 @@ DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
+-- Table `mydb`.`Transcriptoma`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`Transcriptoma` ;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`Transcriptoma` (
+  `ID` VARCHAR(50) NOT NULL,
+  `Longitud` INT NULL,
+  `num_exons` INT NULL,
+  `Gen_ID` INT NOT NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `fk_Transcriptoma_Gen1_idx` (`Gen_ID` ASC) VISIBLE,
+  CONSTRAINT `fk_Transcriptoma_Gen1`
+    FOREIGN KEY (`Gen_ID`)
+    REFERENCES `mydb`.`Gen` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Proteina`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`Proteina` ;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`Proteina` (
+  `ID` VARCHAR(45) NOT NULL,
+  `Nombre` VARCHAR(50) NULL,
+  `Longitud` INT NULL,
+  `Descripcion` VARCHAR(500) NULL,
+  `Transcriptoma_ID` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `fk_Proteina_Transcriptoma1_idx` (`Transcriptoma_ID` ASC) VISIBLE,
+  CONSTRAINT `fk_Proteina_Transcriptoma1`
+    FOREIGN KEY (`Transcriptoma_ID`)
+    REFERENCES `mydb`.`Transcriptoma` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
 -- Table `mydb`.`Pacientes`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`Pacientes` ;
+
 CREATE TABLE IF NOT EXISTS `mydb`.`Pacientes` (
-  `DNI` VARCHAR(9) NOT NULL,
-  `Clinica` VARCHAR(45) NULL DEFAULT NULL,
-  `Nombre` VARCHAR(45) NULL DEFAULT NULL,
-  `Apellidos` VARCHAR(45) NULL DEFAULT NULL,
-  `Edad` INT NULL DEFAULT NULL,
-  `Pais` VARCHAR(45) NULL DEFAULT NULL,
-  `Ciudad` VARCHAR(45) NULL DEFAULT NULL,
+  `DNI` VARCHAR(10) NOT NULL,
+  `Clinica` VARCHAR(45) NULL,
+  `Nombre` VARCHAR(45) NULL,
+  `Apellidos` VARCHAR(45) NULL,
+  `Edad` INT NULL,
+  `Pais` VARCHAR(45) NULL,
+  `Ciudad` VARCHAR(45) NULL,
   `Mutacion_ID` VARCHAR(45) NOT NULL,
   `Mutacion_Gen_ID` INT NOT NULL,
   `Mutacion_Efecto_Fenotipo` VARCHAR(100) NOT NULL,
@@ -83,44 +131,10 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Pacientes` (
   INDEX `fk_Pacientes_Mutacion1_idx` (`Mutacion_ID` ASC, `Mutacion_Gen_ID` ASC, `Mutacion_Efecto_Fenotipo` ASC) VISIBLE,
   CONSTRAINT `fk_Pacientes_Mutacion1`
     FOREIGN KEY (`Mutacion_ID` , `Mutacion_Gen_ID` , `Mutacion_Efecto_Fenotipo`)
-    REFERENCES `mydb`.`Mutacion` (`ID` , `Gen_ID` , `Efecto_Fenotipo`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Transcriptoma`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Transcriptoma` (
-  `ID` VARCHAR(50) NOT NULL,
-  `Longitud` INT NULL DEFAULT NULL,
-  `num_exons` INT NULL DEFAULT NULL,
-  `Gen_ID` INT NOT NULL,
-  PRIMARY KEY (`ID`),
-  INDEX `fk_Transcriptoma_Gen1_idx` (`Gen_ID` ASC) VISIBLE,
-  CONSTRAINT `fk_Transcriptoma_Gen1`
-    FOREIGN KEY (`Gen_ID`)
-    REFERENCES `mydb`.`Gen` (`ID`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Proteina`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Proteina` (
-  `ID` VARCHAR(45) NOT NULL,
-  `Nombre` VARCHAR(50) NULL DEFAULT NULL,
-  `Longitud` INT NULL DEFAULT NULL,
-  `Descripcion` VARCHAR(500) NULL DEFAULT NULL,
-  `Transcriptoma_ID` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`ID`),
-  INDEX `fk_Proteina_Transcriptoma1_idx` (`Transcriptoma_ID` ASC) VISIBLE,
-  CONSTRAINT `fk_Proteina_Transcriptoma1`
-    FOREIGN KEY (`Transcriptoma_ID`)
-    REFERENCES `mydb`.`Transcriptoma` (`ID`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+    REFERENCES `mydb`.`Mutacion` (`ID` , `Gen_ID` , `Efecto_Fenotipo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
--- queries/query2.sql
+++ queries/query2.sql
@@ -1,6 +1,9 @@
-SELECT Gen.Nombre, Proteina.Nombre
-FROM Gen, Mutacion, Proteina
+SELECT Gen.Nombre AS Gene_Symbol, Gen.Longitud AS Longitud_Gen,
+	Proteina.Nombre AS Nombre_Proteina
+FROM Gen, Mutacion, Proteina, Transcriptoma
 WHERE  Mutacion.Tipo = "Missense/nonsense"
-	AND Gen.ID = Proteina.Gen_ID
+	AND Gen.ID = Transcriptoma.Gen_ID
 	AND Gen.ID = Mutacion.Gen_ID
-    AND longitud <= 500;
+    AND Transcriptoma.ID = Proteina.Transcriptoma_ID
+    AND Gen.Longitud <= 200
+ORDER BY Gen.Longitud ASC;
