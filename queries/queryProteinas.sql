62da3c75aa6c7735553559bc8b7bb4070d13ee38
davidcubillos0211 <100205678+davidcubillos0211@users.noreply.github.com>
2022-04-28 12:05:22 +0200

    añadidos force index

--- queries/queryProteinas.sql
+++ queries/queryProteinas.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT Proteina.Nombre, Efecto_Fenotipo
 FROM Proteina, Mutacion
-WHERE Longitud >= 700 AND Transcriptoma_ID IN (SELECT ID FROM Transcriptoma
-	WHERE num_exons > 5 AND Gen_ID IN (SELECT Gen_ID FROM Mutacion
+WHERE Longitud >= 700 AND Transcriptoma_ID IN (SELECT ID FROM Transcriptoma FORCE INDEX (exones)
+	WHERE num_exons > 5 AND Gen_ID IN (SELECT Gen_ID FROM Mutacion FORCE INDEX (tipo)
 		WHERE Tipo = "Splicing"))
