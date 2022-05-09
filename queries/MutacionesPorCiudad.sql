c022603aec9b54bf5df98e3910b973ef7703376d
davidcubillos0211 <100205678+davidcubillos0211@users.noreply.github.com>
2022-05-09 18:43:45 +0200

    paciente

--- queries/MutacionesPorCiudad.sql
+++ queries/MutacionesPorCiudad.sql
@@ -1,5 +1,5 @@
-SELECT Pacientes.Nombre, Pacientes.Ciudad, Efecto.Fenotipo, Efecto.Descripcion AS 'Descripcion enfermedad'
-From Pacientes, Efecto  
- LEFT JOIN Mutacion
+SELECT Paciente.Nombre, Paciente.Ciudad, Efecto.Fenotipo, Efecto.Descripcion AS 'Descripcion enfermedad'
+From Paciente, Efecto  
+LEFT JOIN Mutacion
 ON Mutacion.Efecto_Fenotipo = Efecto.Fenotipo
-Where Pacientes.Ciudad= "Madrid" AND Pacientes.Mutacion_Efecto_Fenotipo=Efecto.Fenotipo ;
+Where Paciente.Ciudad= "Madrid" AND Paciente.Mutacion_Efecto_Fenotipo=Efecto.Fenotipo ;
