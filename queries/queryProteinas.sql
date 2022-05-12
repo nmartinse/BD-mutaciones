ffa63423a79eb8c54810b4c021067bc120a9d3b8
nmartin <nmartins@uma.es>
2022-05-12 19:34:21 +0200

    12.05

--- queries/queryProteinas.sql
+++ queries/queryProteinas.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT Proteina.Nombre, Efecto_Fenotipo
-FROM Proteina, Mutacion
-WHERE Longitud >= 700 AND Transcriptoma_ID IN (SELECT ID FROM Transcriptoma FORCE INDEX (exones)
-	WHERE num_exons > 5 AND Gen_ID IN (SELECT Gen_ID FROM Mutacion FORCE INDEX (tipo)
-		WHERE Tipo = "Splicing"))
+FROM Proteina FORCE INDEX (idx_Longitud), Mutacion
+WHERE Longitud >= 1700 AND Transcriptoma_ID  IN (SELECT ID FROM Transcriptoma 
+	WHERE num_exons > 20 AND Gen_ID IN (SELECT  Gen_ID FROM Mutacion
+		WHERE Tipo = "Splicing")) 
